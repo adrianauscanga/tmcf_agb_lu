7391db5558ecafb9dabbdb2c4835c49ceaaf26fc
adrianauscanga <adrianau@uoregon.edu>
2022-04-14 10:55:28 -0700

    wrote data as csv files and made loop to read all 300 plots

--- 00_organize_data.R
+++ 00_organize_data.R
@@ -1,22 +1,22 @@
-#---------------------------------------------------------------------------------#
-#                             00. Organize data                         
+#---------------------------- 00. Organize data --------------------------
 #
 # Code for editing and organizing the datasets that will be used in the analysis 
 # This script will read in raw data from the input directory, and write clean
 # datasets to the output directory
 #
-#---------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------#
 
-# Load packages
+
+# Load packages -----------------------------------------------------------
 
 library(tidyverse)
 library(plyr)
 library(dplyr)
 library(lubridate)
+library(fs)
 
-####################################################################################
-#                       REMOTE SENSING DATA                                        #
-####################################################################################
+
+# REMOTE SENSING DATA -----------------------------------------------------
 
 # Import plot coordinates:
 
@@ -85,6 +85,44 @@ ci2_l5 <- ci2_l5 %>%
   separate(scene, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
   mutate(date = ymd(date))
 
+# EVI:
+
+evi_l5 <- read_csv("input/RS/evi_cfplots_all.csv", col_names = FALSE)
+head(evi_l5)
+
+evi_l5 <- evi_l5%>%
+  transmute(plot_id = as.character(X6),
+            scene = as.character(X1),
+            lon = as.numeric(X2),
+            lat = as.numeric(X3),
+            sat_time = as.numeric(X4),
+            evi = as.numeric(X5))
+
+head(evi_l5)
+
+evi_l5 <- evi_l5 %>%
+  separate(scene, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
+  mutate(date = ymd(date))
+
+# NDWI:
+
+ndwi_l5 <- read_csv("input/RS/ndwi_cfplots_all.csv", col_names = FALSE)
+head(ndwi_l5)
+
+ndwi_l5 <- ndwi_l5%>%
+  transmute(plot_id = as.character(X6),
+            scene = as.character(X1),
+            lon = as.numeric(X2),
+            lat = as.numeric(X3),
+            sat_time = as.numeric(X4),
+            ndwi = as.numeric(X5))
+
+head(ndwi_l5)
+
+ndwi_l5 <- ndwi_l5 %>%
+  separate(scene, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
+  mutate(date = ymd(date))
+
 # Repeat with landsat 7:
 
 # NDVI:
@@ -140,34 +178,170 @@ ci2_l7 <- ci2_l7 %>%
   separate(scene, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
   mutate(date = ymd(date))
 
+# EVI:
+
+evi_l7 <- read_csv("input/RS/evil7_cfplots_all.csv", col_names = FALSE)
+head(evi_l7)
+
+evi_l7 <- evi_l7%>%
+  transmute(plot_id = as.character(X6),
+            scene = as.character(X1),
+            lon = as.numeric(X2),
+            lat = as.numeric(X3),
+            sat_time = as.numeric(X4),
+            evi = as.numeric(X5))
+
+evi_l7 <- evi_l7 %>%
+  separate(scene, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
+  mutate(date = ymd(date))
+
+# NDWI:
+
+ndwi_l7 <- read_csv("input/RS/ndwil7_cfplots_all.csv", col_names = FALSE)
+head(ndwi_l7)
+
+ndwi_l7 <- ndwi_l7%>%
+  transmute(plot_id = as.character(X6),
+            scene = as.character(X1),
+            lon = as.numeric(X2),
+            lat = as.numeric(X3),
+            sat_time = as.numeric(X4),
+            ndwi = as.numeric(X5))
+
+ndwi_l7 <- ndwi_l7 %>%
+  separate(scene, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
+  mutate(date = ymd(date))
+
+# Add time series that have been filtered with cloud mask
+
+# Landsat 5
+
+ndvinc_ts_files <- "input/RS/l5_noclouds/"  # file directory
+ndvinc_l5_ts <- ndvinc_ts_files %>%   # make a list of all files
+  dir_ls() %>%
+  map(
+    .f = function(path) {
+      read_csv(
+        path,
+        col_names = c("id", "lon", "lat", "sat_time", "ndvinc", "plot_id"),
+        col_types = cols(
+          plot_id = col_character(),
+          id = col_character(),
+          lon = col_double(),
+          lat = col_double(),
+          sat_time = col_double(),
+          ndvinc = col_double()
+        )
+      )
+    }
+  )
+
+ndvinc_l5_ts_tbl <- ndvinc_l5_ts %>%
+  set_names(dir_ls(ndvinc_ts_files)) %>%
+  bind_rows(.id = "file_path")
+  
+dim(ndvinc_l5_ts_tbl)
+
+ndvinc_l5_ts_tbl <- ndvinc_l5_ts_tbl %>%
+  separate(id, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
+  mutate(date = ymd(date)) %>%
+  select(-file_path)
+
+# Landsat 7
+
+ndvincl7_ts_files <- "input/RS/l7_noclouds/"  # file directory
+ndvinc_l7_ts <- ndvincl7_ts_files %>%   # make a list of all files
+  dir_ls() %>%
+  map(
+    .f = function(path) {
+      read_csv(
+        path,
+        col_names = c("id", "lon", "lat", "sat_time", "ndvinc", "plot_id"),
+        col_types = cols(
+          plot_id = col_character(),
+          id = col_character(),
+          lon = col_double(),
+          lat = col_double(),
+          sat_time = col_double(),
+          ndvinc = col_double()
+        )
+      )
+    }
+  )
+
+ndvinc_l7_ts_tbl <- ndvinc_l7_ts %>%
+  set_names(dir_ls(ndvincl7_ts_files)) %>%
+  bind_rows(.id = "file_path")
+
+dim(ndvinc_l7_ts_tbl)
+
+ndvinc_l7_ts_tbl <- ndvinc_l7_ts_tbl %>%
+  separate(id, into = c("landsat", "row_path", "date"), sep = "_", remove = TRUE) %>%
+  mutate(date = ymd(date)) %>%
+  select(-file_path)
+
 # Bind all datasets.
-# First join ndvi, ci1 and ci1, then bind landsat 5 and landsat 7 data
+# First join ndvi, evi, ndwi, ndvinc, ci1 and ci1, then bind landsat 5 and landsat 7 data
 
-l5 <- left_join(ndvi_l5, ci1_l5, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
-  left_join(ci2_l5, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time"))
+l5 <- left_join(ndvi_l5, ndvinc_l5_ts_tbl, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(ci1_l5, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(ci2_l5, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(evi_l5, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(ndwi_l5, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) 
 
-l7 <- left_join(ndvi_l7, ci1_l7, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
-  left_join(ci2_l7, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time"))
+head(l5)
+
+l7 <- left_join(ndvi_l7, ndvinc_l7_ts_tbl, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(ci1_l7, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(ci2_l7, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(evi_l7, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) %>%
+  left_join(ndwi_l7, by = c("plot_id", "landsat", "row_path", "date", "lon", "lat", "sat_time")) 
 
 time_series <- bind_rows(l5, l7) %>%
-  arrange(plot_id, date)
+   arrange(plot_id, date)
 
-time_series %>%
-  filter(is.na(ndvi)) %>%
-  group_by(landsat) %>%
-  dplyr::summarize(na_number = n())
+head(time_series)
 
-# Most NA in dataset are landsat 7 (perhaps due to l7 stripes)
+ time_series %>%
+   filter(is.na(ndvi)) %>%
+   group_by(landsat) %>%
+   dplyr::summarize(na_number = n())
+ 
+# Most NA in dataset are landsat 7 (perhaps due to l7 stripes and because there's more l7 data in general)
 
 # Get rid of duplicated dates
 
-time_series <- time_series %>% # 187,961
-  filter(!is.na(ndvi)) %>% #157,652 rows without NAs
-  group_by(plot_id, date) %>%
-  dplyr::summarize(date = unique(date),
-                   landsat = unique(landsat),
-                   lon = mean(lon),
-                   lat = mean(lat),
-                   ndvi = mean(ndvi),
-                   ci1 = mean(ci1),
-                   ci2 = mean(ci2)) #145,771 rows
+time_series <- time_series %>%
+   distinct(plot_id, date, .keep_all= TRUE) 
+
+# Edit ndvinc column
+
+time_series <- time_series %>%
+  mutate(cloud_mask = ifelse(is.na(ndvinc), "cloud", "no_cloud")) %>%
+  select(-ndvinc)
+
+head(time_series)
+
+# Export long dataset as csv file
+
+save(time_series, file = "output/time_series.RData")
+
+write.csv(time_series, file = "output/time_series.csv")
+
+# Split dataset into individual plots and export dataset that way
+
+time_series_plots <- split(time_series, time_series$plot_id)
+
+time_series_plots2 <- time_series_plots[-c(1:143)]
+
+lapply(names(time_series_plots2), function(x){
+  write_csv(time_series_plots2[[x]], path = paste("output/ts_plots/", x, ".csv", sep = ""))
+})
+
+# FI DATA -----------------------------------------------------
+# 
+# load("input/FI/plots_cf4.RData")
+# head(plots_cf)
+# 
+# load("input/FI/trees_cf4.RData")
+# head(trees_cf)
---
+++ 01_bfast_analysis.R
@@ -0,0 +1,93 @@
+#---------------------------- 01. BFAST analysis --------------------------
+#
+# 
+#
+#-------------------------------------------------------------------------#
+
+# Load packages and files -------------------------------------------------
+
+
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(bfast)
+library(tseries)
+
+# Make a loop to read all ts plots at once
+
+ts_plots <- dir_ls(path="output/ts_plots/")
+
+#2. Make a for loop for reading each file txt file
+#First, create empty variables
+table <- NULL
+names <- NULL
+
+#loop for reading all plots:
+for (i in ts_plots){
+  #Read data
+  table <- read_csv(i, col_names = T) 
+  
+  assign(paste0("p",substr(i,17,23)),table) #assign name "p+plot_id" to table
+  names<- c(names,paste0("p",substr(i,17,23))) #make a list of all plots
+}
+
+
+# Remove cloudy pixels ----------------------------------------------------
+
+
+
+
+# Run bfast ---------------------------------------------------------------
+
+# Loop for running bfast
+
+ts <- NULL
+ndvi <- NULL
+date <- NULL
+ts_list <- NULL
+ts_tsp <- NULL
+ts_h <- NULL
+freq <- NULL
+length_ts <- NULL
+bfastx <- NULL
+bfastlist <- NULL
+bfastoutput <- NULL
+lastiter <- NULL
+breaks <- NULL
+breaks2 <- NULL
+totalbreaks <- NULL
+sumbreaks <- NULL
+bfast_breaks <- NULL
+nobreaks <- NULL
+break_moments <- NULL
+
+for(i in names){
+  plotx <- mget(i) #search and "call" i(which is already in the environment)
+  plotx <- data.frame(plotx)
+  ndvi <- plotx[,8]
+  date <- plotx[,4]
+  ts <- bfastts(ndvi, date, type = "irregular")
+  ts <- na.remove(ts)
+  ts_tsp <- attr(ts, "tsp")
+  freq <- ts_tsp[3]
+  length_ts <- length(ts)
+  ts_h <- (freq/length_ts)*1
+  bfastx <- bfast(ts, h = ts_h, season = "dummy", max.iter = 10, breaks = NULL, hpc = "none", level = 0.05, type = "OLS-MOSUM") 
+  assign(paste0("bfast_",i), bfastx)
+  bfastlist <- c(bfastlist, paste0("bfast_",i)) #create a list of bfast files 
+  bfastoutput <- bfastx$output #get output of bfast
+  lastiter <- tail(bfastoutput, n = 1) #get the last iteration 
+  #breaks <- lastiter[[1]]$bp.Vt #get breakpoint info from last iteration
+  breaks<- lastiter[[c(1,5)]]
+  if(is.na(breaks)) {
+    nobreaks <- c(nobreaks, paste0("bfast_",i),0)
+  } else {
+    breaks2 <- breaks$breakpoints #get only the breakpoints
+    break_moments <- c(break_moments, paste0("bfast_",i,",",breaks2))
+    totalbreaks <- length(breaks2) #calculate no. of breakpoints
+    bfast_breaks <- c(bfast_breaks, paste0("bfast_",i), totalbreaks)
+  }
+  
+}
+
